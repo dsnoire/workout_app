836cb1a2d672b67d9fd641861800b29052d788ff
Kuba <kosinski.jakub01@gmail.com>
2023-12-27 06:53:34 +0100

    Add PopupMenu with delete, edit actions for WorkoutTile

--- lib/features/workout/widgets/workout_list_tile.dart
+++ lib/features/workout/widgets/workout_list_tile.dart
@@ -115,59 +115,5 @@ class WorkoutListTile extends StatelessWidget {
         ],
       ),
     );
-    return ListTile(
-      onTap: () => context.push(
-        '/edit-workout',
-        extra: workout,
-      ),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            flex: 4,
-            child: Text(
-              workout.name,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Text(
-              workout.schedule.name,
-              textAlign: TextAlign.end,
-              style: Theme.of(context).textTheme.labelSmall,
-            ),
-          ),
-        ],
-      ),
-      subtitle: Padding(
-        padding: const EdgeInsets.only(top: 18),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: List.generate(
-            workout.weekdays.length,
-            (index) {
-              final isSelected = workout.weekdays.values.elementAt(index);
-              final color = workout.color.entries
-                  .firstWhere((element) => element.value == true)
-                  .key;
-              return CircleAvatar(
-                backgroundColor:
-                    isSelected == true ? Color(color) : const Color(0xFF131313),
-                child: Text(
-                  workout.weekdays.keys.getWeekdayAbbreviation(index: index),
-                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                        fontWeight: isSelected ? FontWeight.bold : null,
-                      ),
-                ),
-              );
-            },
-          ),
-        ),
-      ),
-    );
   }
 }
