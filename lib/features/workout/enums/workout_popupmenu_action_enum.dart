8ca669db39d8b47a04042b3faacdab86f9315395
Kuba <kosinski.jakub01@gmail.com>
2023-12-27 06:52:03 +0100

    Add PopupMenu with delete, edit actions for WorkoutTile

---
+++ lib/features/workout/enums/workout_popupmenu_action_enum.dart
@@ -0,0 +1,4 @@
+enum WorkoutPopupMenuActionEnum {
+  edit,
+  remove,
+}
--- lib/features/workout/widgets/workout_list_tile.dart
+++ lib/features/workout/widgets/workout_list_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:workout_app/core/constants/app_colors.dart';
+import 'package:workout_app/features/workout/cubits/workout_cubit/workout_cubit.dart';
+import 'package:workout_app/features/workout/enums/workout_popupmenu_action_enum.dart';
 import 'package:workout_app/features/workout/extensions/weekday_abbreviation.dart';
-import 'package:workout_app/features/workout/views/add_or_edit_workout.dart';
 
 import '../models/workout.dart';
 
@@ -15,6 +18,103 @@ class WorkoutListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 22,
+        vertical: 16,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.contrastColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                workout.name,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              PopupMenuButton<WorkoutPopupMenuActionEnum>(
+                iconSize: 19,
+                color: const Color(0xFF131313),
+                elevation: 0,
+                onSelected: (WorkoutPopupMenuActionEnum action) async {
+                  if (action == WorkoutPopupMenuActionEnum.edit) {
+                    context.push(
+                      '/edit-workout',
+                      extra: workout,
+                    );
+                  } else if (action == WorkoutPopupMenuActionEnum.remove) {
+                    await context
+                        .read<WorkoutCubit>()
+                        .removeWorkout(id: workout.id);
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return <PopupMenuEntry<WorkoutPopupMenuActionEnum>>[
+                    const PopupMenuItem<WorkoutPopupMenuActionEnum>(
+                      value: WorkoutPopupMenuActionEnum.edit,
+                      child: Row(
+                        children: [
+                          Icon(Icons.edit),
+                          SizedBox(width: 9),
+                          Text('Edit'),
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem<WorkoutPopupMenuActionEnum>(
+                      value: WorkoutPopupMenuActionEnum.remove,
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete),
+                          SizedBox(width: 9),
+                          Text('Remove'),
+                        ],
+                      ),
+                    ),
+                  ];
+                },
+              )
+            ],
+          ),
+          Text(
+            workout.schedule.name,
+            textAlign: TextAlign.end,
+            style: Theme.of(context).textTheme.labelSmall,
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(
+              workout.weekdays.length,
+              (index) {
+                final isSelected = workout.weekdays.values.elementAt(index);
+                final color = workout.color.entries
+                    .firstWhere((element) => element.value == true)
+                    .key;
+                return CircleAvatar(
+                  backgroundColor: isSelected == true
+                      ? Color(color)
+                      : const Color(0xFF131313),
+                  child: Text(
+                    workout.weekdays.keys.getWeekdayAbbreviation(index: index),
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          fontWeight: isSelected ? FontWeight.bold : null,
+                        ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
     return ListTile(
       onTap: () => context.push(
         '/edit-workout',
